9a9573051195a2ac0fc751ed7ba57dae49d4525d
The Harry Code <theharrycode@gmail.com>
2024-05-20 13:02:05 -0500

    Se agrega el campo edad

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ print("Su Direccion es "+direccion)
 telefono = input("Ingrese su telefono")
 print("Su Telefeno es "+telefono)
 
+edad = input("Ingrese su edad")
+print("Su Edad es "+edad)
+
 #Debería continuar con el curso de Pyhton?
 #Ustedes que dicen?, los leo en los comentarios
 #Adicionalmente, como les parece este tipo de contenido, donde en un solo video, se explican muchos temas.
