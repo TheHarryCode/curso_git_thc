1bc455aa5529a8e49666e2370345f61836884ce0
The Harry Code <theharrycode@gmail.com>
2024-05-20 12:56:20 -0500

    Se agrega campo telefono y se remueve comentario

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ print("Su nombre es "+name)
 lastname = input("Ingrese su apellido")
 print("Su apellido es "+lastname)
 
-#The Harry Code hizo esto
 direccion = input("Ingrese su direccion")
 print("Su Direccion es "+direccion)
+
+telefono = input("Ingrese su telefono")
+print("Su Telefeno es "+telefono)
 
 #Debería continuar con el curso de Pyhton?
